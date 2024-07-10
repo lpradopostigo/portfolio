13ec1a30003bfb57a565faf676b63488a32e3fc9
Luis Prado <lpradopostigo@gmail.com>
2024-07-10 12:39:46 -0500

    improve content, improve print style

--- app/App.jsx
+++ app/App.jsx
@@ -77,43 +77,56 @@ export default function App() {
           <a href="#about-me">
             <Icon name="brackets-square-fill"></Icon>
           </a>
-          <div>
+          <div class="sections-links">
             <a href="#work-experience">career</a>
             <a href="#side-projects">relevant experience</a>
+            <a href="#languages">languages</a>
+            <a href="#technologies">technologies</a>
           </div>
         </nav>
       </header>
 
       <main>
         <section id="about-me" class="about-me">
-          <img src="profile.jpg" alt="profile" loading="lazy" />
+          <img
+            class="about-me__image"
+            src="profile.jpg"
+            alt="profile"
+            loading="lazy"
+          />
 
-          <div>
+          <div class="about-me__info">
             <h1>Luis Angel Prado Postigo</h1>
 
             <div class="about-me__title">Software Developer</div>
 
+            <a href="mailto:lpradopostigo@gmail.com">
+              <Icon name="envelope-simple-regular" size="24"></Icon>
+              <span class="only-print">lpradopostigo@gmail.com</span>
+            </a>
             <a
               href="https://www.linkedin.com/in/luis-angel-prado-postigo-813916231"
               target="_blank"
             >
               <Icon name="linkedin-logo-regular" size="24"></Icon>
+              <span class="only-print">linkedin</span>
             </a>
 
             <a href="https://github.com/lpradopostigo" target="_blank">
               <Icon name="github-logo-regular" size="24"></Icon>
-            </a>
-
-            <a href="mailto:lpradopostigo@gmail.com">
-              <Icon name="envelope-simple-regular" size="24"></Icon>
+              <span class="only-print">github</span>
             </a>
           </div>
 
           <p>
-            With over 3 years of professional experience, I have expertise in
-            both frontend and backend technologies. My technical skill set
-            includes JavaScript, TypeScript, React, Node.js, Golang, Docker, and
-            PostgreSQL, among others.
+            I worked in different projects for different companies as frontend
+            developer and backend developer for more than 3 years.
+          </p>
+
+          <p>
+            As a person, I constantly trying to learn from my mistakes, not to
+            be perfect but to avoid being too flawed. In my free time, I enjoy
+            creating tools to solve my own problems, and also composing music.
           </p>
         </section>
 
@@ -126,6 +139,22 @@ export default function App() {
           <h2>Relevant Experience</h2>
 
           <Timeline data={sideProjectsData}></Timeline>
+        </section>
+
+        <section id="languages" class="languages">
+          <h2>Languages</h2>
+          English (C1), Spanish (Native)
+        </section>
+
+        <section id="technologies" class="technologies">
+          <h2>Technologies</h2>
+
+          <p>
+            JavaScript, TypeScript, React, Node.js, Go, Docker, PostgreSQL,
+            MySQL, Git, GitLab, Rust, C, C++, MariaDB, SQLite, SolidJS, Vue,
+            CSS, HTML, SASS, Webpack, Vite, Astro, Jest, Linux, Rollup, Postman,
+            Figma, AdobeXD
+          </p>
         </section>
 
         <footer>
